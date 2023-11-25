c3047b1edf94ba4b82036632d6e30c5061e12e6c
Michal <salatman13@gmail.com>
2023-11-25 17:43:18 +0100

    first commit

---
+++ src/main/java/com/example/individualprojectfe/IndividualProjectFeApplication.java
@@ -0,0 +1,13 @@
+package com.example.individualprojectfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualProjectFeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndividualProjectFeApplication.class, args);
+    }
+
+}
